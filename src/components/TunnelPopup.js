e0f41f0c1e5cb399731b4e8562db2a45bc7e0990
Beck-berry <trebeka98@gmail.com>
2023-06-04 13:31:50 +0200

    Visualize conditional values on highways #25 - popup fix

--- src/components/TunnelPopup.js
+++ src/components/TunnelPopup.js
@@ -3,16 +3,18 @@ import {Popup} from "react-map-gl";
 import opening_hours from "opening_hours";
 import {SimpleOpeningHours} from "simple-opening-hours";
 import {Table, TableBody, TableCell, TableRow, Typography} from "@mui/material";
+import moment from "moment";
+import 'moment/locale/nb';
 
-const DAYS = {
-	"Mo": "mandag",
-	"Tu": "tirsdag",
-	"We": "onsdag",
-	"Th": "torsdag",
-	"Fr": "fredag",
-	"Sa": "lørdag",
-	"Su": "søndag"
-}
+const DAYS = new Map([
+	["mo", "mandag"],
+	["tu", "tirsdag"],
+	["we", "onsdag"],
+	["th", "torsdag"],
+	["fr", "fredag"],
+	["sa", "lørdag"],
+	["su", "søndag"]
+]);
 
 class TunnelPopup extends React.Component {
 	
@@ -26,7 +28,7 @@ class TunnelPopup extends React.Component {
 		}
 		
 		if (oh === "24/7") {
-			return startString + DAYS['Mo'] + " til " + DAYS['Su'] + "." ;
+			return startString + DAYS['mo'] + " til " + DAYS['su'] + "." ;
 		}
 		
 		// display opening hours for the next 24 hours
@@ -41,7 +43,8 @@ class TunnelPopup extends React.Component {
 	}
 	
 	getTime(fullDate) {
-		return fullDate.toLocaleTimeString();
+		moment.locale('nb');
+		return moment(fullDate).format('LT');
 	}
 	
 	getMessage(point) {
@@ -84,50 +87,34 @@ class TunnelPopup extends React.Component {
 		// If there are multiple rules.
 		if (oh.split(";").length - 1 > 1) {
 			const ohObject = new SimpleOpeningHours(oh);
-			return ohObject.getTable();
+			const openingHours = ohObject.getTable();
+			
+			let rows = [];
+			DAYS.forEach((value, key) => {
+				rows.push(
+					<TableRow>
+						<TableCell>{value}</TableCell>
+						<TableCell>{openingHours[key]}</TableCell>
+					</TableRow>
+				);
+			})
+			
+			return (
+				<Table padding={'none'} size={'small'}>
+					<TableBody>
+						{rows}
+					</TableBody>
+				</Table>
+			);
 		}
 		return null;
 	}
 	
 	render() {
-		const openingHours = this.getOpeningHoursTable(this.props.point)
 		return (
 			<Popup latitude={this.props.lngLat.lat} longitude={this.props.lngLat.lng} onClose={this.props.onClose}>
 				<Typography gutterBottom={true}>{this.getMessage(this.props.point)}</Typography>
-				{openingHours != null &&
-					<Table padding={'none'} size={'small'}>
-						<TableBody>
-							<TableRow>
-								<TableCell>{DAYS['Mo']}</TableCell>
-								<TableCell>{openingHours['mo']}</TableCell>
-							</TableRow>
-							<TableRow>
-								<TableCell>{DAYS['Tu']}</TableCell>
-								<TableCell>{openingHours['tu']}</TableCell>
-							</TableRow>
-							<TableRow>
-								<TableCell>{DAYS['We']}</TableCell>
-								<TableCell>{openingHours['we']}</TableCell>
-							</TableRow>
-							<TableRow>
-								<TableCell>{DAYS['Th']}</TableCell>
-								<TableCell>{openingHours['th']}</TableCell>
-							</TableRow>
-							<TableRow>
-								<TableCell>{DAYS['Fr']}</TableCell>
-								<TableCell>{openingHours['fr']}</TableCell>
-							</TableRow>
-							<TableRow>
-								<TableCell>{DAYS['Sa']}</TableCell>
-								<TableCell>{openingHours['sa']}</TableCell>
-							</TableRow>
-							<TableRow>
-								<TableCell>{DAYS['Su']}</TableCell>
-								<TableCell>{openingHours['su']}</TableCell>
-							</TableRow>
-						</TableBody>
-					</Table>
-				}
+				{this.getOpeningHoursTable(this.props.point)}
 			</Popup>
 		);
 	}
