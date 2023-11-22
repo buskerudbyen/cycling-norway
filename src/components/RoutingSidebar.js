accdc371e0066c47f68dc8142c918a2b492db7b3
Beck-berry <trebeka98@gmail.com>
2023-11-22 22:46:19 +0100

    Enhance display of elevation data #92

--- src/components/RoutingSidebar.js
+++ src/components/RoutingSidebar.js
@@ -3,12 +3,33 @@ import SearchField from "./SearchField";
 import TimerIcon from '@mui/icons-material/Timer';
 import HeightIcon from '@mui/icons-material/Height';
 import ExpandIcon from '@mui/icons-material/Expand';
+import {Box, Modal, Typography} from "@mui/material";
+import {Chart as ChartJS, registerables} from "chart.js";
+import {Line} from "react-chartjs-2";
 
 class Menu extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			showElevationPopup: false
+		}
 		this.chooseStart = this.chooseStart.bind(this);
 		this.chooseDest = this.chooseDest.bind(this);
+		this.showElevationPopup = this.showElevationPopup.bind(this);
+		this.hideElevationPopup = this.hideElevationPopup.bind(this);
+		
+		ChartJS.register(...registerables);
+		
+		this.style = {
+			position: 'absolute',
+			top: '50%',
+			left: '50%',
+			transform: 'translate(-50%, -50%)',
+			bgcolor: 'background.paper',
+			border: '1px solid #000',
+			boxShadow: 24,
+			p: 2,
+		};
 	}
 	
 	chooseStart(event, value) {
@@ -17,6 +38,18 @@ class Menu extends React.Component {
 	
 	chooseDest(event, value) {
 		this.props.chooseDest(event, value);
+	}
+	
+	showElevationPopup() {
+		this.setState({
+			showElevationPopup: true
+		})
+	}
+	
+	hideElevationPopup() {
+		this.setState({
+			showElevationPopup: false
+		})
 	}
 	
 	render() {
@@ -28,17 +61,54 @@ class Menu extends React.Component {
 		}
 		
 		return (
-			<div id="routing" hidden={this.props.hidden}>
-				<div id="searchFields">
-					<SearchField onChoose={this.chooseStart} labelText="Fra" />
-					<SearchField onChoose={this.chooseDest} labelText="Til" />
+			<>
+				<div id="routing" hidden={this.props.hidden}>
+					<div id="searchFields">
+						<SearchField onChoose={this.chooseStart} labelText="Fra" />
+						<SearchField onChoose={this.chooseDest} labelText="Til" />
+					</div>
+					<div id="routingResults" hidden={!this.props.duration}>
+						<TimerIcon htmlColor={"gray"} fontSize={"small"} /><span>{duration}</span>
+						<HeightIcon htmlColor={"gray"} sx={{ transform: 'rotate(90deg)' }} /><span>{distance} km</span>
+						<span onMouseOver={this.showElevationPopup}>
+							<ExpandIcon htmlColor={"gray"} />
+							{elevation} m
+						</span>
+					</div>
 				</div>
-				<div id="routingResults" hidden={!this.props.duration}>
-					<TimerIcon htmlColor={"gray"} fontSize={"small"} /><span>{duration}</span>
-					<HeightIcon htmlColor={"gray"} sx={{ transform: 'rotate(90deg)' }} /><span>{distance} km</span>
-					<ExpandIcon htmlColor={"gray"} /><span>{elevation} m</span>
-				</div>
-			</div>
+				<Modal id={"elevationInfo"} aria-labelledby="modal-title" open={this.state.showElevationPopup} onClose={this.hideElevationPopup}>
+					<Box sx={this.style} className="modal-box">
+						<Typography id="modal-title" variant="h6" component="h2">
+							Elevation info
+						</Typography>
+						<Line
+							type="line"
+							datasetIdKey='id'
+							data={{
+								labels: this.props.elevationProfile,
+								datasets: [
+									{
+										id: 1,
+										label: '',
+										data: this.props.elevationProfile,
+										fill: 'origin'
+									}
+								],
+							}}
+							options={{
+								legend: {
+									display: false
+								},
+								scales: {
+									x: {
+										display: false
+									}
+								},
+							}}
+						/>
+					</Box>
+				</Modal>
+			</>
 		);
 	}
 }
