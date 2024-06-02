cce91be9bc6f47f8aa9b3bf62653857ad5b0eada
Beck-berry <trebeka98@gmail.com>
2024-06-02 18:10:14 +0200

    Update help text #129

--- src/components/menu/ButtonHelp.tsx
+++ src/components/menu/ButtonHelp.tsx
@@ -41,18 +41,18 @@ const ButtonHelp = () => {
       >
         <Box sx={style} className="modal-box">
           <Typography id="modal-title" variant="h6" component="h2">
-            Sykkelkart- og sykkelvei-pilot 2021-2023
+            Sykkelkart- og sykkelvei-pilot 2021-2024
           </Typography>
           <Typography
             className="modal-description"
             sx={{ mt: 2 }}
             align="justify"
           >
-            Få en anbefalt god sykkelrute fra hvilken som helst vei eller
-            skogssti i Norge til et sted du ønsker å reise til. Klikk først på
-            hvor du vil reise fra, og klikk igjen for å velge destinasjon.
-            Start- og stoppmarkørene kan også da flyttes til nye steder.
-            Ruteforslaget vil da oppdateres.{" "}
+            Få en anbefalt god sykkelrute fra hvilken som helst vei eller skogssti i 
+            Norge til et sted du ønsker å reise til. Søk opp steder og adresser eller 
+            klikk først på hvor du vil reise fra, og klikk igjen for å velge destinasjon. 
+            Start- og stoppmarkørene kan også da flyttes til nye steder. Ruteforslaget 
+            vil da oppdateres.{" "}
             <a href="./?from=59.71982%2C10.25855&to=59.74460%2C10.20589#13.35/59.73268/10.22634">
               Vis eksempel.
             </a>
@@ -77,10 +77,11 @@ const ButtonHelp = () => {
             <a href="https://www.buskerudbyen.no/">Buskerudbyen</a>,{" "}
             <a href="https://bymiljopakken.no/">Bymiljøpakken</a> /{" "}
             <a href="https://www.kolumbus.no/">Kolumbus</a>, &nbsp;
-            <a href="https://viken.no/">Viken fylkeskommune</a> og{" "}
+            <a href="https://viken.no/">Viken fylkeskommune</a>,
             <a href="https://www.vegvesen.no/fag/trafikk/its-portalen/its-i-statens-vegvesen/">
               Statens vegvesen (ITS-programmet)
-            </a>
+            </a>og{" "}
+            <a href="https://bypakka.no/">Bypakke Grenland</a>
             . Takk også til alle frivillige bidragsytere av{" "}
             <a href="https://wiki.openstreetmap.org/wiki/No:Main_Page">
               OpenStreetMap
@@ -88,7 +89,7 @@ const ButtonHelp = () => {
             og alle åpne kartdata fra Statens vegvesen og Kartverket. Løsningen
             utviklet av <a href="https://leonard.io/">Leonard</a> og{" "}
             <a href="https://github.com/Beck-berry">Beck-berry</a>. Sist
-            oppdatert mars 2023.
+            oppdatert mai 2024.
           </Typography>
         </Box>
       </Modal>
